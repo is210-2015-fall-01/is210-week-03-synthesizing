9244ea72d4fcb65ecc76e020440a8685941c8242
Saulat Majid <solyraja@hotmail.com>
2015-09-11 02:13:01 +0000

    tasks 1 to 5 complete.

--- task_02.py
+++ task_02.py
@@ -4,3 +4,10 @@
 
 
 import inquisition
+START_LOC = inquisition.SPANISH.index('Spanish')
+PRE_STRING = inquisition.SPANISH[0:START_LOC]
+STR_LEN = len(inquisition.SPANISH)
+POST_STRING = inquisition.SPANISH[START_LOC + len('Spanish'):STR_LEN]
+FLEMISH = PRE_STRING + 'Flemish' + POST_STRING
+
+print FLEMISH
