451ad72df3998197f3a84abbab33d0c293556ace
Grace Park <pjeanne14@gmail.com>
2015-09-14 03:59:52 +0000

    The pylint test returns nothing so it passes. However, the nosetest for  task 4 says that it fails. Must adjust.

--- task_04.py
+++ task_04.py
@@ -6,3 +6,6 @@ NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
+A = '{:06d}'.format(RNUM)
+EMAIL = NEWS.format(NTYPE, A, friend=FNAME)
+print EMAIL
