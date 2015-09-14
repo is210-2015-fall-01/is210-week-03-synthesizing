a5cd1275115ae2546c50c9731392f4276ffe4e39
Christopher Fernanders <cj.fernanders1986@gmail.com>
2015-09-14 04:53:01 +0000

    task 1 and 4

--- task_01.py
+++ task_01.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Contains expectations."""
-
 import inquisition
 FISHY = inquisition.SPANISH.replace('surprise', 'haddock')
 print FISHY
 print inquisition.SPANISH
-
