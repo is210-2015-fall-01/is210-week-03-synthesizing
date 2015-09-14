93d4503156ac60dac2e776098e712cad33f5c1b7
Grace Park <pjeanne14@gmail.com>
2015-09-14 03:55:54 +0000

    Used index, slicing len and concatenation for task 2 Used replace for task 1

--- task_02.py
+++ task_02.py
@@ -4,3 +4,10 @@
 
 
 import inquisition
+
+FIRST = inquisition.SPANISH.index('Spanish')
+SECOND = len('Spanish')
+THIRD = inquisition.SPANISH[:FIRST]
+FOURTH = inquisition.SPANISH[(FIRST+SECOND):]
+FIFTH = 'Flemish'
+FLEMISH = str(THIRD) + str(FIFTH) + str(FOURTH)
