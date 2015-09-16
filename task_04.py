01efe5fcc4f8673c465a7c3211e109a6f2d54026
Grace Park <pjeanne14@gmail.com>
2015-09-16 02:40:50 +0000

    Nosetests finally passed. I change the RNUM to be included into A and then inputted into the format statement. The nosetest still failed and then for no reason, decided to pass.

--- task_04.py
+++ task_04.py
@@ -3,10 +3,15 @@
 """Provides variables for formatting."""
 
 NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
-EMAIL = NEWS.format('*amazing*', '{:06d}'.format(42), friend='Pat')
+RNUM = 42
+A = ('{0:06d}'.format(RNUM))
+EMAIL = NEWS.format('*amazing*', A, friend='Pat')
 print EMAIL
 
 # RNUM = 42
-# A = '{:06d}'.format(RNUM)
+# A = '{0:06d}'.format(RNUM)
 # FNAME = 'Pat'
 # NTYPE = '*amazing*'
+# A = '{0:06d}'.format(RNUM)
+# When I replace 42 with 5678, I get the correct 005678, just like the
+# nosetests says I should get. Nosetest passed!!! Yay.
